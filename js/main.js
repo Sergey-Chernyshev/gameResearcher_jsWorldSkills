4e0ee170a48d49d0e2a02fca69367152bbc02b87
Sergey-Chernyshev <gebodiksch@gmail.com>
2023-04-06 19:34:18 +0300

    1

--- js/main.js
+++ js/main.js
@@ -3,11 +3,16 @@ const usernameIn = document.querySelector("#username")
 const startBtn = document.querySelector(".btn-start")
 
 // main content
-const usernameMain = document.querySelector(".username-block")
+const usernameMain = document.querySelector("#username-block")
 const timer = document.querySelector("#timer-count")
 const gasoline = document.querySelector(".gasoline-number")
 
 const player = document.querySelector("#player")
+const back1 = document.querySelector("#b1")
+const back2 = document.querySelector("#b2")
+
+
+let walls = document.querySelectorAll(".wall")
 
 let hiMenu = startBtn.addEventListener("click", () => {
     console.log(usernameIn.value)
@@ -26,8 +31,35 @@ function startGame(username, window){
     window.classList.add("close")
     window.classList.remove("modal")
 
+    usernameMain.innerText = username
+
     player.style.left = "13%"
     player.style.top = "50%"
+
+    back1.style.left = "0%"
+    back2.style.left = "100%"
+
+
+
+    let left = 1024
+    for (let i = 0; i < walls.length; i++) {
+        let wall = walls[i];
+        changeWall(wall)
+        wall.style.left = left+i*300 + "px"
+
+    }
+
+    function randomIntFromInterval(min, max) { // min and max included
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    var randHeight
+    function randPos(){
+        randHeight = randomIntFromInterval(100, 500)
+        randpos = randomIntFromInterval(1, 2)
+        return [randHeight, randpos]
+    }
+
 
     let sec = 0
     let min = 0
@@ -67,6 +99,8 @@ function startGame(username, window){
 
     function pouse() {
         stopTimer()
+        stopBack()
+        stopWall()
         document.removeEventListener("keydown", keyCheck)
         document.addEventListener("keydown", resEv)
     }
@@ -78,6 +112,8 @@ function startGame(username, window){
             document.removeEventListener("keydown", resEv)
             document.addEventListener("keydown", keyCheck)
             startTimer()
+            startBack()
+            startWall(walls)
             console.log("Resume")
         }
     }
@@ -88,16 +124,66 @@ function startGame(username, window){
         backgroundInterval = setInterval(backAnimation, 10)
     }
 
+    function stopBack() {
+        // background
+        clearInterval(backgroundInterval)
+    }
+
     function backAnimation() {
+        back1.style.left = parseFloat(back1.style.left) - 0.2 + "%";
+        back2.style.left = parseFloat(back2.style.left) - 0.2 + "%";
+        if (parseFloat(back1.style.left) <= -100) {
+            back1.style.left = "100%"
+        }
+        if (parseFloat(back2.style.left) <= -100) {
+            back2.style.left = "100%"
+        }
+    }
+
+
+    var wallInterval
+    function startWall() {
+        // background
+        wallInterval = setInterval(wallAnimation, 10)
+    }
+
+    function stopWall() {
+        // background
+        clearInterval(wallInterval)
+    }
+
+    function changeWall(wall){
+        // pouse()
+        wall.style.left = "1324px"
+        let = [wallHeight, pos] = randPos()
+        wall.style.height = wallHeight + "px"
+        console.log(pos, wallHeight)
+        if (pos == 1) {
+            wall.style.top = "0px"
+            wall.style.bottom = null
+        }
+        if (pos == 2) {
+            wall.style.bottom = "0px"
+            wall.style.top = null
+        }
         
+    }
+    let listBl = []
+    function wallAnimation() {
+        for (let i = 0; i < walls.length; i++) {
+            let wall = walls[i];
+            wall.style.left = parseFloat(wall.style.left) - 2.1 + "px";
+            if (parseFloat(wall.style.left) <= 46) {
+                changeWall(wall)
+            }
+        }
     }
 
 
 
     document.addEventListener("keydown", keyCheck)
     startTimer()
-    backAnimation()
-    
+    startBack()
+    startWall(walls)
 
-    
 }
